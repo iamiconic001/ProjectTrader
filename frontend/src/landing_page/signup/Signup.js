1a89dba5e3c70f0e9a9ee028bf9bb24e7e31fbaf
iconic <chiranjeevgautam559@gmail.com>
2024-07-28 15:19:17 +0530

    Updated routes

--- frontend/src/landing_page/signup/Signup.js
+++ frontend/src/landing_page/signup/Signup.js
@@ -46,7 +46,8 @@ const Signup = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:3002/signup",
+        
+          `${process.env.REACT_APP_BACKEND_URL}/signup`,
         {
           ...inputValue,
         },
