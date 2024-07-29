d555d788e6f40f504352915a9eea2d2c4c2c800c
iconic <chiranjeevgautam559@gmail.com>
2024-07-29 13:01:23 +0530

    Some updation

--- frontend/src/landing_page/login/Login.js
+++ frontend/src/landing_page/login/Login.js
@@ -2,22 +2,30 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
-import { useAuth } from "../AuthContext.js"; // Assuming AuthContext provides login function
+import { useAuth } from '../AuthContext.js';
 import Navbar from "../Navbar";
 import Footer from "../Footer";
 
 const Login = () => {
   const [showAlert, setShowAlert] = useState(false);
   const { login } = useAuth(); // Use login function from the context
-  const navigate = useNavigate();
 
+  const handleClick = () => {
+    // Toggle showAlert state to true when button is clicked
+    setShowAlert(true);
+
+    // Optionally, you can reset showAlert after a certain time
+    setTimeout(() => {
+      setShowAlert(false);
+    }, 10000); // Hide alert after 100 seconds (adjust as needed)
+  };
+
+  const navigate = useNavigate();
   const [inputValue, setInputValue] = useState({
     email: "",
     password: "",
   });
-
   const { email, password } = inputValue;
-
   const handleOnChange = (e) => {
     const { name, value } = e.target;
     setInputValue({
@@ -30,27 +38,16 @@ const Login = () => {
     toast.error(err, {
       position: "bottom-left",
     });
-
   const handleSuccess = (msg) =>
     toast.success(msg, {
       position: "bottom-left",
     });
 
-  const handleClick = () => {
-    // Toggle showAlert state to true when button is clicked
-    setShowAlert(true);
-
-    // Optionally, you can reset showAlert after a certain time
-    setTimeout(() => {
-      setShowAlert(false);
-    }, 10000); // Hide alert after 100 seconds (adjust as needed)
-  };
-
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      console.log("Email value:", email);
-      console.log("Password value:", password);
+      console.log('Email value:', inputValue.email);
+      console.log('Password value:', inputValue.password);
       const { data } = await axios.post(
         `${process.env.REACT_APP_BACKEND_URL}/login`,
         { ...inputValue },
@@ -71,9 +68,8 @@ const Login = () => {
       console.error(error);
       handleError("An unexpected error occurred.");
     }
-
-    // Reset input fields after submission
     setInputValue({
+      ...inputValue,
       email: "",
       password: "",
     });
@@ -82,29 +78,17 @@ const Login = () => {
   return (
     <>
       <Navbar />
-      <div
-        className="form_container text-center"
-        style={{ paddingTop: "80px", paddingBottom: "100px" }}
-      >
-        <h2 style={{ paddingBottom: "40px" }}>Login Account</h2>
+      <div className="form_container text-center" style={{paddingTop:"80px", paddingBottom:"100px"}}>
+        <h2 style={{paddingBottom:"40px"}}>Login Account</h2>
         <form onSubmit={handleSubmit}>
           <div>
-            <label
-              htmlFor="email"
-              style={{
-                paddingBottom: "20px",
-                marginRight: "15px",
-                fontSize: "1.25em",
-              }}
-            >
-              Email
-            </label>
+            <label htmlFor="email" style={{paddingBottom:"20px", marginRight:"15px",fontSize:"1.25em"}}>Email</label>
             <input
               style={{
                 color: "black",
                 border: "2px solid black",
                 borderRadius: "4px",
-                fontSize: "1.01em",
+                fontSize:"1.01em",
                 height: "40px",
                 width: "300px",
               }}
@@ -116,22 +100,13 @@ const Login = () => {
             />
           </div>
           <div>
-            <label
-              htmlFor="password"
-              style={{
-                paddingBottom: "20px",
-                marginRight: "15px",
-                fontSize: "1.25em",
-              }}
-            >
-              Password
-            </label>
+            <label htmlFor="password" style={{paddingBottom:"20px", marginRight:"15px",fontSize:"1.25em"}}>Password</label>
             <input
               style={{
                 color: "black",
                 border: "2px solid black",
                 borderRadius: "4px",
-                fontSize: "1.01em",
+                fontSize:"1.01em",
                 height: "40px",
                 width: "300px",
               }}
@@ -142,7 +117,7 @@ const Login = () => {
               onChange={handleOnChange}
             />
           </div>
-          <div style={{ paddingBottom: "40px", paddingTop: "20px" }}>
+          <div style={{paddingBottom:"40px",paddingTop:"20px"}}>
             {showAlert && (
               <div className="alert alert-success" role="alert">
                 Login Successful , Please Explore our Zerodha !{" "}
@@ -157,6 +132,7 @@ const Login = () => {
                     width: "80px",
                   }}
                 >
+                  {" "}
                   <Link
                     style={{
                       textDecoration: "none",
@@ -171,19 +147,15 @@ const Login = () => {
                 </button>
               </div>
             )}
-            <button
-              style={{
+            <button style={{
                 color: "white",
                 border: "1px solid black",
-                fontSize: "1.2em",
+                fontSize:"1.2em",
                 borderRadius: "4px",
                 backgroundColor: "blue",
                 height: "40px",
                 width: "80px",
-              }}
-              type="submit"
-              onClick={handleClick}
-            >
+              }} type="submit" onClick={handleClick}>
               Submit
             </button>
           </div>
@@ -199,6 +171,7 @@ const Login = () => {
                 width: "80px",
               }}
             >
+              {" "}
               <Link
                 style={{
                   textDecoration: "none",
@@ -222,6 +195,7 @@ const Login = () => {
                 marginLeft: "20px",
               }}
             >
+              {" "}
               <Link
                 style={{
                   textDecoration: "none",
@@ -234,7 +208,6 @@ const Login = () => {
                 Explore{" "}
               </Link>{" "}
             </button>
-            `
           </span>
         </form>
         <ToastContainer />
@@ -245,224 +218,3 @@ const Login = () => {
 };
 
 export default Login;
-
-// import React, { useState } from "react";
-// import { Link, useNavigate } from "react-router-dom";
-// import axios from "axios";
-// import { ToastContainer, toast } from "react-toastify";
-// import { useAuth } from '../AuthContext.js';
-// import Navbar from "../Navbar";
-// import Footer from "../Footer";
-
-// const Login = () => {
-//   const [showAlert, setShowAlert] = useState(false);
-//   const { login } = useAuth(); // Use login function from the context
-
-//   const handleClick = () => {
-//     // Toggle showAlert state to true when button is clicked
-//     setShowAlert(true);
-
-//     // Optionally, you can reset showAlert after a certain time
-//     setTimeout(() => {
-//       setShowAlert(false);
-//     }, 10000); // Hide alert after 100 seconds (adjust as needed)
-//   };
-
-//   const navigate = useNavigate();
-//   const [inputValue, setInputValue] = useState({
-//     email: "",
-//     password: "",
-//   });
-//   const { email, password } = inputValue;
-//   const handleOnChange = (e) => {
-//     const { name, value } = e.target;
-//     setInputValue({
-//       ...inputValue,
-//       [name]: value,
-//     });
-//   };
-
-//   const handleError = (err) =>
-//     toast.error(err, {
-//       position: "bottom-left",
-//     });
-//   const handleSuccess = (msg) =>
-//     toast.success(msg, {
-//       position: "bottom-left",
-//     });
-
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     try {
-//       console.log('Email value:', inputValue.email);
-//       console.log('Password value:', inputValue.password);
-//       const { data } = await axios.post(
-//         `${process.env.REACT_APP_BACKEND_URL}/login`,
-//         { ...inputValue },
-//         { withCredentials: true }
-//       );
-//       console.log(data);
-//       const { success, message } = data;
-//       if (success) {
-//         handleSuccess(message);
-//         login(); // Set login state to true on successful login
-//         setTimeout(() => {
-//           navigate("/");
-//         }, 1000);
-//       } else {
-//         handleError(message);
-//       }
-//     } catch (error) {
-//       console.error(error);
-//       handleError("An unexpected error occurred.");
-//     }
-//     setInputValue({
-//       ...inputValue,
-//       email: "",
-//       password: "",
-//     });
-//   };
-
-//   return (
-//     <>
-//       <Navbar />
-//       <div className="form_container text-center" style={{paddingTop:"80px", paddingBottom:"100px"}}>
-//         <h2 style={{paddingBottom:"40px"}}>Login Account</h2>
-//         <form onSubmit={handleSubmit}>
-//           <div>
-//             <label htmlFor="email" style={{paddingBottom:"20px", marginRight:"15px",fontSize:"1.25em"}}>Email</label>
-//             <input
-//               style={{
-//                 color: "black",
-//                 border: "2px solid black",
-//                 borderRadius: "4px",
-//                 fontSize:"1.01em",
-//                 height: "40px",
-//                 width: "300px",
-//               }}
-//               type="email"
-//               name="email"
-//               value={email}
-//               placeholder="Enter your email"
-//               onChange={handleOnChange}
-//             />
-//           </div>
-//           <div>
-//             <label htmlFor="password" style={{paddingBottom:"20px", marginRight:"15px",fontSize:"1.25em"}}>Password</label>
-//             <input
-//               style={{
-//                 color: "black",
-//                 border: "2px solid black",
-//                 borderRadius: "4px",
-//                 fontSize:"1.01em",
-//                 height: "40px",
-//                 width: "300px",
-//               }}
-//               type="password"
-//               name="password"
-//               value={password}
-//               placeholder="Enter your password"
-//               onChange={handleOnChange}
-//             />
-//           </div>
-//           <div style={{paddingBottom:"40px",paddingTop:"20px"}}>
-//             {showAlert && (
-//               <div className="alert alert-success" role="alert">
-//                 Login Successful , Please Explore our Zerodha !{" "}
-//                 <button
-//                   className="text-center"
-//                   style={{
-//                     color: "white",
-//                     border: "1px solid black",
-//                     borderRadius: "4px",
-//                     backgroundColor: "green",
-//                     height: "40px",
-//                     width: "80px",
-//                   }}
-//                 >
-//                   {" "}
-//                   <Link
-//                     style={{
-//                       textDecoration: "none",
-//                       fontSize: "1.2em",
-//                       color: "white",
-//                     }}
-//                     to={"/"}
-//                   >
-//                     {" "}
-//                     Explore{" "}
-//                   </Link>{" "}
-//                 </button>
-//               </div>
-//             )}
-//             <button style={{
-//                 color: "white",
-//                 border: "1px solid black",
-//                 fontSize:"1.2em",
-//                 borderRadius: "4px",
-//                 backgroundColor: "blue",
-//                 height: "40px",
-//                 width: "80px",
-//               }} type="submit" onClick={handleClick}>
-//               Submit
-//             </button>
-//           </div>
-//           <span>
-//             Already have an account?{" "}
-//             <button
-//               style={{
-//                 color: "white",
-//                 border: "1px solid black",
-//                 borderRadius: "4px",
-//                 backgroundColor: "green",
-//                 height: "40px",
-//                 width: "80px",
-//               }}
-//             >
-//               {" "}
-//               <Link
-//                 style={{
-//                   textDecoration: "none",
-//                   fontSize: "1.2em",
-//                   color: "white",
-//                 }}
-//                 to={"/signup"}
-//               >
-//                 Signup
-//               </Link>{" "}
-//             </button>
-//             <button
-//               className="text-center"
-//               style={{
-//                 color: "white",
-//                 border: "1px solid black",
-//                 borderRadius: "4px",
-//                 backgroundColor: "green",
-//                 height: "40px",
-//                 width: "80px",
-//                 marginLeft: "20px",
-//               }}
-//             >
-//               {" "}
-//               <Link
-//                 style={{
-//                   textDecoration: "none",
-//                   fontSize: "1.2em",
-//                   color: "white",
-//                 }}
-//                 to={"/"}
-//               >
-//                 {" "}
-//                 Explore{" "}
-//               </Link>{" "}
-//             </button>
-//           </span>
-//         </form>
-//         <ToastContainer />
-//       </div>
-//       <Footer />
-//     </>
-//   );
-// };
-
-// export default Login;
--- frontend/src/landing_page/signup/Signup.js
+++ frontend/src/landing_page/signup/Signup.js
@@ -7,15 +7,24 @@ import Footer from "../Footer";
 
 const Signup = () => {
   const [showAlert, setShowAlert] = useState(false);
-  const navigate = useNavigate();
 
+  const handleClick = () => {
+    // Toggle showAlert state to true when button is clicked
+    setShowAlert(true);
+
+    // Optionally, you can reset showAlert after a certain time
+    setTimeout(() => {
+      setShowAlert(false);
+    }, 7000); // Hide alert after 3 seconds (adjust as needed)
+  };
+
+  const navigate = useNavigate();
   const [inputValue, setInputValue] = useState({
     email: "",
     password: "",
     username: "",
   });
   const { email, password, username } = inputValue;
-
   const handleOnChange = (e) => {
     const { name, value } = e.target;
     setInputValue({
@@ -28,27 +37,20 @@ const Signup = () => {
     toast.error(err, {
       position: "bottom-left",
     });
-
   const handleSuccess = (msg) =>
     toast.success(msg, {
       position: "bottom-right",
     });
-  const handleClick = () => {
-    // Toggle showAlert state to true when button is clicked
-    setShowAlert(true);
-
-    // Optionally, you can reset showAlert after a certain time
-    setTimeout(() => {
-      setShowAlert(false);
-    }, 7000); // Hide alert after 7 seconds (adjust as needed)
-  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        ${process.env.REACT_APP_BACKEND_URL}/signup,
-        { ...inputValue },
+        
+          `${process.env.REACT_APP_BACKEND_URL}/signup`,
+        {
+          ...inputValue,
+        },
         { withCredentials: true }
       );
       const { success, message } = data;
@@ -63,32 +65,47 @@ const Signup = () => {
       }
     } catch (error) {
       console.log(error);
-      handleError("An unexpected error occurred.");
     }
-
-    // Reset input fields after submission
     setInputValue({
+      ...inputValue,
       email: "",
       password: "",
       username: "",
     });
   };
- return (
+
+  return (
     <>
       <Navbar />
-      <div className="form_container text-center" style={{ paddingTop: "80px", paddingBottom: "100px" }}>
-        <h2 style={{ paddingBottom: "40px" }}>Signup Account</h2>
+      <div className="form_container text-center" style={{paddingTop:"80px", paddingBottom:"100px"}}>
+        <h2 style={{paddingBottom:"40px"}}>Signup Account</h2>
         <form onSubmit={handleSubmit}>
           <div>
-            <label htmlFor="username" style={{ paddingBottom: "20px", marginRight: "15px", fontSize: "1.25em" }}>
-              Username
-            </label>
+            <label htmlFor="email" style={{paddingBottom:"20px", marginRight:"15px",fontSize:"1.25em"}}>Email</label>
             <input
               style={{
                 color: "black",
                 border: "2px solid black",
                 borderRadius: "4px",
-                fontSize: "1.01em",
+                fontSize:"1.01em",
+                height: "40px",
+                width: "300px",
+              }}
+              type="email"
+              name="email"
+              value={email}
+              placeholder="Enter your email"
+              onChange={handleOnChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="email" style={{paddingBottom:"20px", marginRight:"15px",fontSize:"1.25em"}}>Username</label>
+            <input
+              style={{
+                color: "black",
+                border: "2px solid black",
+                borderRadius: "4px",
+                fontSize:"1.01em",
                 height: "40px",
                 width: "300px",
               }}
@@ -100,38 +117,17 @@ const Signup = () => {
             />
           </div>
           <div>
-            <label htmlFor="email" style={{ paddingBottom: "20px", marginRight: "15px", fontSize: "1.25em" }}>
-              Email
-            </label>
+            <label htmlFor="password" style={{paddingBottom:"20px", marginRight:"15px",fontSize:"1.25em"}}>Password</label>
             <input
               style={{
                 color: "black",
                 border: "2px solid black",
                 borderRadius: "4px",
-                fontSize: "1.01em",
+                fontSize:"1.01em",
+                
                 height: "40px",
                 width: "300px",
-                }}
-              type="email"
-              name="email"
-              value={email}
-              placeholder="Enter your email"
-              onChange={handleOnChange}
-            />
-          </div>
-          <div>
-            <label htmlFor="password" style={{ paddingBottom: "20px", marginRight: "15px", fontSize: "1.25em" }}>
-              Password
-            </label>
-            <input
-              style={{
-                color: "black",
-                border: "2px solid black",
-                borderRadius: "4px",
-                fontSize: "1.01em",
-                height: "40px",
-                width: "300px",
-                }}
+              }}
               type="password"
               name="password"
               value={password}
@@ -139,25 +135,21 @@ const Signup = () => {
               onChange={handleOnChange}
             />
           </div>
-          <div style={{ paddingBottom: "40px", paddingTop: "20px" }}>
+          <div style={{paddingBottom:"40px",paddingTop:"20px"}}>
             {showAlert && (
               <div className="alert alert-success" role="alert">
-                Signup successfully done! Now Please Login to your account!
+                Signup successfully done ! Now Please Login to your account !
               </div>
             )}
-            <button
-              style={{
+            <button style={{
                 color: "white",
                 border: "1px solid black",
-                fontSize: "1.2em",
+                fontSize:"1.2em",
                 borderRadius: "4px",
                 backgroundColor: "blue",
-                 height: "40px",
+                height: "40px",
                 width: "80px",
-              }}
-              type="submit"
-              onClick={handleClick}
-            >
+              }} type="submit" onClick={handleClick}>
               Submit
             </button>
           </div>
@@ -173,6 +165,7 @@ const Signup = () => {
                 width: "70px",
               }}
             >
+              {" "}
               <Link
                 style={{
                   textDecoration: "none",
@@ -196,6 +189,7 @@ const Signup = () => {
                 marginLeft: "20px",
               }}
             >
+              {" "}
               <Link
                 style={{
                   textDecoration: "none",
@@ -218,226 +212,3 @@ const Signup = () => {
 };
 
 export default Signup;
-
-
-
-
-
-
-
-
-
-// import React, { useState } from "react";
-// import { Link, useNavigate } from "react-router-dom";
-// import axios from "axios";
-// import { ToastContainer, toast } from "react-toastify";
-// import Navbar from "../Navbar";
-// import Footer from "../Footer";
-
-// const Signup = () => {
-//   const [showAlert, setShowAlert] = useState(false);
-
-//   const handleClick = () => {
-//     // Toggle showAlert state to true when button is clicked
-//     setShowAlert(true);
-
-//     // Optionally, you can reset showAlert after a certain time
-//     setTimeout(() => {
-//       setShowAlert(false);
-//     }, 7000); // Hide alert after 3 seconds (adjust as needed)
-//   };
-
-//   const navigate = useNavigate();
-//   const [inputValue, setInputValue] = useState({
-//     email: "",
-//     password: "",
-//     username: "",
-//   });
-//   const { email, password, username } = inputValue;
-//   const handleOnChange = (e) => {
-//     const { name, value } = e.target;
-//     setInputValue({
-//       ...inputValue,
-//       [name]: value,
-//     });
-//   };
-
-//   const handleError = (err) =>
-//     toast.error(err, {
-//       position: "bottom-left",
-//     });
-//   const handleSuccess = (msg) =>
-//     toast.success(msg, {
-//       position: "bottom-right",
-//     });
-
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     try {
-//       const { data } = await axios.post(
-        
-//           `${process.env.REACT_APP_BACKEND_URL}/signup`,
-//         {
-//           ...inputValue,
-//         },
-//         { withCredentials: true }
-//       );
-//       const { success, message } = data;
-//       if (success) {
-//         handleSuccess(message);
-//         console.log("SUCCESS");
-//         setTimeout(() => {
-//           navigate("/");
-//         }, 1000);
-//       } else {
-//         handleError(message);
-//       }
-//     } catch (error) {
-//       console.log(error);
-//     }
-//     setInputValue({
-//       ...inputValue,
-//       email: "",
-//       password: "",
-//       username: "",
-//     });
-//   };
-
-//   return (
-//     <>
-//       <Navbar />
-//       <div className="form_container text-center" style={{paddingTop:"80px", paddingBottom:"100px"}}>
-//         <h2 style={{paddingBottom:"40px"}}>Signup Account</h2>
-//         <form onSubmit={handleSubmit}>
-//           <div>
-//             <label htmlFor="email" style={{paddingBottom:"20px", marginRight:"15px",fontSize:"1.25em"}}>Email</label>
-//             <input
-//               style={{
-//                 color: "black",
-//                 border: "2px solid black",
-//                 borderRadius: "4px",
-//                 fontSize:"1.01em",
-//                 height: "40px",
-//                 width: "300px",
-//               }}
-//               type="email"
-//               name="email"
-//               value={email}
-//               placeholder="Enter your email"
-//               onChange={handleOnChange}
-//             />
-//           </div>
-//           <div>
-//             <label htmlFor="email" style={{paddingBottom:"20px", marginRight:"15px",fontSize:"1.25em"}}>Username</label>
-//             <input
-//               style={{
-//                 color: "black",
-//                 border: "2px solid black",
-//                 borderRadius: "4px",
-//                 fontSize:"1.01em",
-//                 height: "40px",
-//                 width: "300px",
-//               }}
-//               type="text"
-//               name="username"
-//               value={username}
-//               placeholder="Enter your username"
-//               onChange={handleOnChange}
-//             />
-//           </div>
-//           <div>
-//             <label htmlFor="password" style={{paddingBottom:"20px", marginRight:"15px",fontSize:"1.25em"}}>Password</label>
-//             <input
-//               style={{
-//                 color: "black",
-//                 border: "2px solid black",
-//                 borderRadius: "4px",
-//                 fontSize:"1.01em",
-                
-//                 height: "40px",
-//                 width: "300px",
-//               }}
-//               type="password"
-//               name="password"
-//               value={password}
-//               placeholder="Enter your password"
-//               onChange={handleOnChange}
-//             />
-//           </div>
-//           <div style={{paddingBottom:"40px",paddingTop:"20px"}}>
-//             {showAlert && (
-//               <div className="alert alert-success" role="alert">
-//                 Signup successfully done ! Now Please Login to your account !
-//               </div>
-//             )}
-//             <button style={{
-//                 color: "white",
-//                 border: "1px solid black",
-//                 fontSize:"1.2em",
-//                 borderRadius: "4px",
-//                 backgroundColor: "blue",
-//                 height: "40px",
-//                 width: "80px",
-//               }} type="submit" onClick={handleClick}>
-//               Submit
-//             </button>
-//           </div>
-//           <span>
-//             Already have an account?{" "}
-//             <button
-//               style={{
-//                 color: "white",
-//                 border: "1px solid black",
-//                 borderRadius: "4px",
-//                 backgroundColor: "green",
-//                 height: "35px",
-//                 width: "70px",
-//               }}
-//             >
-//               {" "}
-//               <Link
-//                 style={{
-//                   textDecoration: "none",
-//                   fontSize: "1.2em",
-//                   color: "white",
-//                 }}
-//                 to={"/login"}
-//               >
-//                 Login
-//               </Link>{" "}
-//             </button>{" "}
-//             <button
-//               className="text-center"
-//               style={{
-//                 color: "white",
-//                 border: "1px solid black",
-//                 borderRadius: "4px",
-//                 backgroundColor: "green",
-//                 height: "35px",
-//                 width: "80px",
-//                 marginLeft: "20px",
-//               }}
-//             >
-//               {" "}
-//               <Link
-//                 style={{
-//                   textDecoration: "none",
-//                   fontSize: "1.2em",
-//                   color: "white",
-//                 }}
-//                 to={"/"}
-//               >
-//                 {" "}
-//                 Explore{" "}
-//               </Link>{" "}
-//             </button>
-//           </span>
-//         </form>
-//         <ToastContainer />
-//       </div>
-//       <Footer />
-//     </>
-//   );
-// };
-
-// export default Signup;
